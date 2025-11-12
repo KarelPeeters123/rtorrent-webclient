0c9a7bdb6f8dd3b5d1142712d5c64a62a30d38f1
Karel <karel.peeters97@icloud.com>
2025-11-13 07:15:35 +1300

    all if

--- api.py
+++ api.py
@@ -61,4 +61,4 @@ def index():
 
 if __name__ == "__main__":
     # Default host/port; use a process manager in production
-    app.run(host="127.0.0.1", port=5000)
+    app.run(host="0.0.0.0", port=5000)
